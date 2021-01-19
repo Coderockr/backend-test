8baddf7ea7136f97b7f8d0f15f915165b29fead2
Luis Guerreiro <luiscvlh11@gmail.com>
2021-01-19 06:57:17 +0000

    🎨 Add db service to docker-compose

--- events/settings.py
+++ events/settings.py
@@ -120,8 +120,12 @@ WSGI_APPLICATION = "events.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 

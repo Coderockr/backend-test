9e3b53a75e6ddd42bf5d7141a1dfbf279ce9eb2d
Daniel Freitas <daniel1208x@gmail.com>
2021-02-07 18:43:50 -0300

    relationship details to friends list

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Friend;
-use Illuminate\Http\Request;
 
 class FriendController extends Controller
 {
     public function index()
     {
-        return auth()->user()->friends;
+        return Friend::query()
+            ->with(['user', 'friend',])
+            ->where('user_id', auth()->user()->id)
+            ->get();
     }
 }
--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Friend extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function friend()
+    {
+        return $this->belongsTo(User::class, 'friend_id', 'id');
+    }
 }

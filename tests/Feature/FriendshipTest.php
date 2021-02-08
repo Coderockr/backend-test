45d97ad2f0435671cad7f745075e4011ac1479d9
Daniel Freitas <daniel1208x@gmail.com>
2021-02-07 22:53:28 -0300

    initial friendships tests

---
+++ tests/Feature/FriendshipTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Friendship;
+use App\Models\User;
+
+it('list correctly', function () {
+    $user = User::factory()->create();
+    Friendship::factory()->count(2)->create(['user_id' => $user->id]);
+
+    $this->actingAs($user, 'api')
+        ->get(route('friendships'))
+        ->assertStatus(200)
+        ->assertJson([
+            ['user_id' => $user->id],
+            ['user_id' => $user->id],
+        ]);
+});
+
+it('delete correctly', function () {
+    $user = User::factory()->create();
+    $friendship = Friendship::factory(['user_id' => $user->id])->create();
+
+    $this->actingAs($user, 'api')
+        ->delete(route('friendships.delete', $friendship->id))
+        ->assertStatus(204);
+});

01f032087fd18e860fea8f28b14b7023e635ccd4
vivi <financeiro@portalautopecas.com.br>
2021-02-09 11:54:21 -0300

    add cors

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const multer = require('multer')
     //const multerConfig = require('../config/multer')
+const cors = require('cors')
 const UserController = require('./controller/UserController')
 const EventController = require('./controller/EventsController')
 const UserEventController = require('./controller/UserEventController')
@@ -12,18 +13,18 @@ const upload = multer({
 const routes = Router()
 
 //Rotas para Usuários
-routes.post('/user/create', upload.single('File'), UserController.insertUser)
-routes.get('/user/:page', UserController.index)
+routes.post('/user/create', cors(), upload.single('File'), UserController.insertUser)
+routes.get('/user/:page', cors(), UserController.index)
 
 //Rota para autenticação de usuario
-routes.post('/user/auth', UserController.autenticate)
+routes.post('/user/auth', cors(), UserController.autenticate)
 
 //Rotas para eventos
-routes.post('/event/create', EventController.insert)
-routes.get('/event/:page', EventController.index)
-routes.get('/event/index/:id', EventController.indexDetails)
-routes.get('/event/region/:region', EventController.showRegion)
-routes.get('/event/date/:date', EventController.showDate)
+routes.post('/event/create', cors(), EventController.insert)
+routes.get('/event/:page', cors(), EventController.index)
+routes.get('/event/index/:id', cors(), EventController.indexDetails)
+routes.get('/event/region/:region', cors(), EventController.showRegion)
+routes.get('/event/date/:date', cors(), EventController.showDate)
 
 //Rotas para usuarios ligados a eventos
 routes.post('/userEvent/:userId/:eventId', UserEventController.insert)
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
 const path = require('path')
+const cors = require('cors')
 const connection = require('../config/connection')
 const dotenv = require('dotenv')
+
 
 dotenv.config()
 

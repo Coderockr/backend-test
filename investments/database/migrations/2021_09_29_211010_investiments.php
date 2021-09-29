c293915b200718b2b4eac82b7bd075d72509ccb1
paulo.jesus <paulohenrique.nj@gmail.com>
2021-09-29 18:21:05 -0300

    Add migration investment table

---
+++ investments/database/migrations/2021_09_29_211010_investiments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Investiments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->integer('amount');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+}

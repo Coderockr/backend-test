3de87957262d657a0edd57bc858954a1df433dc1
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-03 14:51:09 -0300

    Fix policy rule

--- investments/app/Policies/UserPolicy.php
+++ investments/app/Policies/UserPolicy.php
@@ -7,18 +7,36 @@ use App\Models\User;
 class UserPolicy
 {
     /**
-     * Determine if the given post can be updated by the user.
+     * Determine if the given action can be used by the user.
+     * 
+     * Admin - Can do all actions on customers accounts
+     * Customer - Can do all action in your own account
      *
      * @param  \App\Models\User  $user
+     * @param  integer $userIdFromAction - user id sent in the request
      * @return bool
      */
-    public function show($user, $var)
+    public function permission($user, $userIdInRequest)
     {
-        dd(
-            $var,
-            $user->id,
-            $user->fetchRole()
-        );
-        // return true;
+        if ($this->userIsAdmin($user)) {
+            return true;
+        }
+
+        if ($this->userInResquestIsTheSameLoggedIn($user, $userIdInRequest)) {
+            return true;
+        }
+
+        return false;
     }
+
+    public function userIsAdmin($user)
+    {
+        return ($user->fetchRole()) ['name'] == 'admin';
+    }
+
+    public function userInResquestIsTheSameLoggedIn($user, $userIdInRequest)
+    {
+        return $user->id == $userIdInRequest;
+    }
+
 }
--- investments/app/Providers/AuthServiceProvider.php
+++ investments/app/Providers/AuthServiceProvider.php
@@ -32,7 +32,7 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        Gate::define('user-show', [UserPolicy::class, 'show']);
+        Gate::define('user-has-permission', [UserPolicy::class, 'permission']);
 
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->input('api_token')) {

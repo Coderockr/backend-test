7b7f39d1c1a0d74a53327e2a75b78e31523faf61
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-03 16:54:18 -0300

    Add base to calculate withdraws

---
+++ investments/src/InvestmentCalculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Investment;
+
+
+class InvestmentCalculator
+{
+    public function investmentBalance($investmentData, $investmentAmount, $withdrawDate, $withdrawAmount)
+    {
+        //// Qual é o balance?
+
+        ///// se o withdrawamount for maior q o balance lance erro
+
+        //// se não vamos registrar o withdraw
+    }
+}
+

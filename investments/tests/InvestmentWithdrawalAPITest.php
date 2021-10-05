019916a1583b426150cff607b26913022e91118c
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-05 20:28:03 -0300

    Adding tests
    Test to verify if attempt to withdrawal more than available balance fails

--- investments/tests/InvestmentWithdrawalAPITest.php
+++ investments/tests/InvestmentWithdrawalAPITest.php
@@ -31,15 +31,99 @@ class InvestmentWithdrawalAPITest extends TestCase
 
     }
 
-    public function createInvestment($token)
+    public function testIfWithdrawalOfInvestmentAlertsAboutExceedsBalance()
+    {
+        $token = $this->JWTtoken('admin');
+
+        $investment = $this->createInvestment(
+            $token,
+            [
+                'date' => '2021-08-10',
+                'amount' => '1000.00'
+            ]
+        );
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-09-11',
+                'type'         => 'partial',
+                'amount'       => '500.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(200);
+
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-09-20',
+                'type'         => 'partial',
+                'amount'       => '500.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(200);
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-10-05',
+                'type'         => 'partial',
+                'amount'       => '500.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(401);
+
+    }
+
+    public function testIfAlertsWhenTryToWithdrawalMoreThenBalance()
+    {
+        $token = $this->JWTtoken('admin');
+
+        $investment = $this->createInvestment(
+            $token,
+            [
+                'date' => '2021-08-10',
+                'amount' => '1000.00'
+            ]
+        );
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-08-15',
+                'type'         => 'partial',
+                'amount'       => '2000.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(401);
+    }
+
+    public function createInvestment($token, $data=[])
     {
         $this->json(
             'POST',
             '/api/v1/investment/create',
             [
-                'owner'  => '1',
-                'date'   => '2021-01-01',
-                'amount' => '1000.00'
+                'owner'  => $data['owner'] ?? '1',
+                'date'   => $data['date'] ?? '2021-01-01',
+                'amount' => $data['amount'] ?? '1000.00'
             ],
             [
                 'HTTP_Authorization' => $token

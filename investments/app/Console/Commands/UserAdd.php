8746a55edfb247edcbcbdd1efafc861d66bf9859
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-05 18:56:47 -0300

    Adding command to add a user on database by CLI.

--- investments/app/Console/Commands/UserAdd.php
+++ investments/app/Console/Commands/UserAdd.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\ApiController;
 use App\Http\Controllers\FileController;
 use App\Content;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Roles;
+use App\Models\User;
+use App\Models\Permissions;
 use Log;
 
 
@@ -19,7 +22,7 @@ class UserAdd extends Command
      *
      * @var string
      */
-    protected $signature = "user:add {name} {email} {password}";
+    protected $signature = "user:add {name} {email} {password} {role}";
 
     /**
      * The console command description.
@@ -37,21 +40,44 @@ class UserAdd extends Command
     public function handle()
     {
 
-        $aux = \DB::table('user')->where(
+        $roleId = $this->roleId($this->argument('role'));
+
+        $aux = User::where(
             'email', '=',  $this->argument('email')
         )->get()
         ->toArray();
 
         if (empty($aux)) {
-            \DB::table('user')->insert(
+            $userId = User::create(
                 [
                     'name' => $this->argument('name'),
                     'email' => $this->argument('email'),
                     'password' => Hash::make($this->argument('password'))
                 ]
             );
+
+            if ($userId) {
+                Permissions::insert(
+                    ['users_id' => $userId->id, 'roles_id' => $roleId]
+                );
+            }
+
+            echo "Created!\n";
         }
 
+    }
+
+    public function roleId($role)
+    {
+        $rolesID = Roles::select('id')->where('name', '=', $role)->get()->toArray();
+
+        if ($rolesID) {
+            return $rolesID[0] ['id'];
+        }
+
+        exit(
+            "Role not found in database - aborting insert\n"
+        );
     }
 }
 

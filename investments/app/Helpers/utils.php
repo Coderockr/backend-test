3633939836304ae8957479bc27346c5f7a11c795
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-05 16:29:25 -0300

    Adding more helper functions

--- investments/app/Helpers/utils.php
+++ investments/app/Helpers/utils.php
@@ -1,13 +1,41 @@
 <?php
 
 
-function transfomrAmountToDB($mount)
+function transfomrAmountToInt($amount)
 {
-    return $mount * 100;
+    if (is_float($amount)) {
+        return intval(multiply100($amount));
+    }
+
+    return $amount;
 }
 
-function transfomrAmountFromDB($mount)
+function multiply100($amount)
 {
-    return $mount / 100;
+    return $amount * 100;
+}
+
+function divide100($amount)
+{
+    return $amount / 100;
+}
+
+
+function transfomrAmountToFloat($amount)
+{
+    if (is_int($amount)) {
+        return floatval(divide100($amount));
+    }
+
+    return $amount;
+}
+
+function formatFloat($amount)
+{
+    if (is_float($amount)) {
+        return number_format($amount, 2);
+    }
+
+    return $amount;
 }
 

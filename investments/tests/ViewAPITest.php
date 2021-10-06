2b6024b0ff3274e446af325ff59f51f6378622cc
paulo.jesus <paulohenrique.nj@gmail.com>
2021-10-05 21:00:29 -0300

    Test if show withdrawal of investment

---
+++ investments/tests/ViewAPITest.php
@@ -0,0 +1,106 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ViewAPITest extends TestCase
+{
+
+    public function testIfViewApiListsWithdrawal()
+    {
+        $token = $this->JWTtoken('admin');
+
+        $investment = $this->createInvestment(
+            $token,
+            [
+                'date' => '2021-08-10',
+                'amount' => '1000.00'
+            ]
+        );
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-09-20',
+                'type'         => 'partial',
+                'amount'       => '200.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(200);
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/withdrawal',
+            [
+                'investmentId' => $investment->data->investment->id,
+                'date'         => '2021-09-20',
+                'type'         => 'partial',
+                'amount'       => '200.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(200);
+
+        $this->json(
+            'POST',
+            '/api/v1/investment/view',
+            [
+                'investmentId' => $investment->data->investment->id,
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        )->seeStatusCode(200);
+
+        $jsonResponse = json_decode($this->response->getContent(), true);
+
+        $this->assertArrayHasKey('investment', $jsonResponse);
+        $this->assertArrayHasKey('balance', $jsonResponse);
+        $this->assertArrayHasKey('withdrawals', $jsonResponse);
+
+    }
+
+
+
+
+    public function createInvestment($token, $data=[])
+    {
+        $this->json(
+            'POST',
+            '/api/v1/investment/create',
+            [
+                'owner'  => $data['owner'] ?? '1',
+                'date'   => $data['date'] ?? '2021-01-01',
+                'amount' => $data['amount'] ?? '1000.00'
+            ],
+            [
+                'HTTP_Authorization' => $token
+            ]
+        );
+
+        return json_decode($this->response->getContent());
+
+    }
+
+    public function JWTtoken($role)
+    {
+        $this->post(
+            '/auth/login',
+            [
+                'email' => $role . '@email.com',
+                'password'=> 'passwd123'
+            ]
+        );
+
+        $r = json_decode($this->response->getContent());
+
+        if ($r->access_token) {
+            return  $r->access_token;
+        }
+    }
+}

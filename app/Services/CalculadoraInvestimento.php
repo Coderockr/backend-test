362c4a0f3d484b6be40def4cb93d1cbcb0a23dd4
lauro.appelt@universo.univates.br <lauro.appelt@universo.univates.br>
2022-02-13 16:49:03 -0300

    criacao metodo para resgate e cálculo do imposto

--- app/Services/CalculadoraInvestimento.php
+++ app/Services/CalculadoraInvestimento.php
@@ -8,18 +8,56 @@ use DateTime;
 class CalculadoraInvestimento {
     const TAXA_MENSAL = 0.0052;
 
-    public function obterMontanteInvestimento(Investimento $investimento):float {
-        //obtém total de meses do investimento até hoje
-        $dataAtual = new DateTime();
-        $dataOrigem = new DateTime($investimento->data);
-        $interval = $dataAtual->diff($dataOrigem);
-        $totalDeMeses = $interval->m + $interval->y * 12;
+    const IMPOSTO_INVESTIMENTO_MENOR_DE_UM_ANO = 0.225;
+    const IMPOSTO_INVESTIMENTO_DE_UM_HA_DOIS_ANOS = 0.185;
+    const IMPOSTO_INVESTIMENTO_MAIOR_DE_DOIS_ANOS = 0.15;
+    
+    public function obterValoresResgate(Investimento $investimento):array {
+        $montante = $this->obterMontanteInvestimento($investimento);
+        $rendimento = $montante - $investimento->valor;
+        $valorImposto = $this->obterValorDoImposto($investimento,  $rendimento);
+
         
+        $valoresResgate = [
+            'valor_total' => $montante,
+            'valor_imposto' => $valorImposto,
+            'valor_real' => $montante - $valorImposto
+        ];
+
+        return $valoresResgate;
+    }
+
+    public function obterMontanteInvestimento(Investimento $investimento):float {
+        $totalDeMeses = $this->obterTotalDeMesesDoInvestimento($investimento->data);
         $capital = $investimento->valor;
         $taxa = self::TAXA_MENSAL;
 
         $montante = $capital * pow((1 + $taxa), $totalDeMeses);
         
         return round($montante, 2);
+    }
+
+    private function obterValorDoImposto(Investimento $investimento,  float $rendimento) {
+        $totalDeMeses = $this->obterTotalDeMesesDoInvestimento($investimento->data);
+        $anos = $totalDeMeses / 12;
+       
+        if ($anos < 1.0) {
+            $taxaImposto = self::IMPOSTO_INVESTIMENTO_MENOR_DE_UM_ANO;
+        } else if ($anos >= 1.0 && $anos < 2.0) {
+            $taxaImposto = self::IMPOSTO_INVESTIMENTO_DE_UM_HA_DOIS_ANOS;
+        } else {
+            $taxaImposto = self::IMPOSTO_INVESTIMENTO_MAIOR_DE_DOIS_ANOS;
+        }
+
+        return round($rendimento * $taxaImposto, 2);
+    }
+
+    private function obterTotalDeMesesDoInvestimento($dataInvestimento):int {
+        $dataAtual = new DateTime();
+        $dataOrigem = new DateTime($dataInvestimento);
+        $interval = $dataAtual->diff($dataOrigem);
+        $totalDeMeses = $interval->m + $interval->y * 12;
+
+        return $totalDeMeses;
     }
 }

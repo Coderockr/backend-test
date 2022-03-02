7224796d8daf4db32e2e93c8f4cb1d1dd5b88c24
Luciano <lucianoeberhardt95@outlook.com>
2022-03-02 13:20:49 -0300

    appropriate response for withdrawn

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator as PaginateList;
 use App\Repositories\Contracts\Investment as InvestmentContract;
 use App\Models\User\Investment;
@@ -34,14 +35,19 @@ class InvestmentController extends Controller
         return $this->investment->findByID($id);
     }
 
-    public function withdraw(Request $request, int $id): bool
+    public function withdraw(Request $request, int $id): JsonResponse
     {
         $investment = $this->investment->findByID($id);
 
         $this->validate($request, [
-            'date' => 'date|date_format:Y-m-d H:i:s|before_or_equal:now|after:'.$investment->created_at
+            'date' => 'date|date_format:Y-m-d H:i:s|before_or_equal:now|after:'.$investment->created_at,
         ]);
 
-        return $this->investment->withdraw( $investment, $request->get('date') );
+        if($investment->withdrawn) 
+            return response()->json(['error' => 'Withdraw has been done!'], 409);
+            
+        $this->investment->withdraw( $investment, $request->get('date') );
+
+        return response()->json('Withdrawned with sucesss!', 200);
     }
 }

99cd0e9490ffe1dcd1b40e1a51444c516896b3bb
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-03-06 05:36:27 -0300

    Add controller method to handle investment overview data

--- app/Controller/Api/Investment.php
+++ app/Controller/Api/Investment.php
@@ -25,4 +25,40 @@ class Investment
         }
         return $investmentList;
     }
+
+
+    public static function getInvestmentOverview($request, $id)
+    {
+        $investmentOverViewList = [];
+        $results = EntityInvestment::getInvestmentOverview("idInvestment = {$id}");
+        
+        while($investment = $results->fetch(\PDO::FETCH_ASSOC))
+        {            
+            $investmentDate = new DateTime($investment['investmentDate']);
+            $lastInvestmentUpdate = new DateTime($investment['lastInvestmentUpdate']);
+            $initialAmount = (float) number_format($investment['initialAmount'], 2, '.', '');
+            $currentAmount = (float) number_format($investment['currentAmount'], 2, '.', '');
+            $currentGain = (float) number_format($investment['totalGain'], 2, '.', '');
+
+            $expectedAmount = (float) number_format(
+                EntityInvestment::calcExpectedAmount($currentAmount)
+                , 2, '.', ''
+            );
+
+            $expectedGain = (float) number_format($expectedAmount - $currentAmount + $currentGain, 2, '.', '');
+
+            $investmentOverViewList[] = [
+                'idInvestment' => (int) $investment['idInvestment'],
+                'investmentDate' => $investmentDate->format('Y-m-d'),
+                'lastInvestmentUpdate' => $lastInvestmentUpdate->format('Y-m-d'),
+                'initialAmount' => $initialAmount,
+                'currentAmount' => $currentAmount,
+                'currentGain' => $currentGain,
+                'expectedAmount' => $expectedAmount,
+                'expectedGain' => $expectedGain,
+                'withdrew' => (bool) $investment['withdrew']
+            ];
+        }
+        return $investmentOverViewList;
+    }
 }

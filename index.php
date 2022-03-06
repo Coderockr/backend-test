5822fb948f61674b05274fd77ecb3f9a2aa1bf57
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-03-06 05:08:14 -0300

    Change index.php file to include the api.php file, routes manager, and the environment variable loader

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-require __DIR__.'vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
+
+use App\Http\Router;
+use Bootstrap\EnvironmentVariableLoader;
+
+EnvironmentVariableLoader::load(__DIR__);
+
+define('URL', getenv('APP_URL'));
+
+$router = new Router(URL);
+
+include __DIR__.'/routes/api.php';
+
+$router->run()->sendResponse();

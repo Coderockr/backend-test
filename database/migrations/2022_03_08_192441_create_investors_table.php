68bd3822f497280feeb3f19b12e361d0fdf75f09
IncogniJM <juliano@julianomiranda.com>
2022-03-08 16:40:50 -0300

    Definição das tabelas
    * Criação da migration das databases investors e investments

--- database/migrations/2022_03_08_192441_create_investors_table.php
+++ database/migrations/2022_03_08_192441_create_investors_table.php
@@ -15,6 +15,7 @@ class CreateInvestorsTable extends Migration
     {
         Schema::create('investors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2022_03_08_192538_create_investments_table.php
+++ database/migrations/2022_03_08_192538_create_investments_table.php
@@ -15,6 +15,15 @@ class CreateInvestmentsTable extends Migration
     {
         Schema::create('investments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('investor_id');
+            $table->decimal('amount_start', 12, 2);
+            $table->date('date_creation');
+            $table->boolean('withdrawn')->default(false);
+            $table->date('date_withdraw')->nullable();
+            $table->decimal('gain', 12, 2)->nullable();
+            $table->decimal('tax', 12, 2)->nullable();
+            $table->decimal('amount_total', 12, 2)->nullable();
+            $table->decimal('amount_withdrawn', 12, 2)->nullable();
             $table->timestamps();
         });
     }

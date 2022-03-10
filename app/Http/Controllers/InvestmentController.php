47c2e61064ec02ecdf2b79aaf68e9fb5c7b87230
Matheus Adler <matheusadler@gmail.com>
2022-03-10 13:23:35 -0300

    add validation to create investment

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Investment as Investment;
 use App\Http\Resources\Investment as InvestmentResource;
+use DateTime;
+use Illuminate\Support\Facades\Validator;
 
 class InvestmentController extends Controller
 {
@@ -38,10 +40,22 @@ class InvestmentController extends Controller
         $investments = new Investment;
         $investments->owner = $request->input('owner');
         $investments->amount = $request->input('amount');
+        $now = new DateTime("now");
         $investments->create_date = $request->input('create_date');
-    
+        
+        $validate = Validator::make($request->all(), [
+            'owner'         => 'required',
+            'amount'        => 'required|numeric|gte:0',
+            'create_date'   => 'required|date|before_or_equal:' . $now->format("Y-m-d")
+        ]);
+
+        if ($validate->fails()) {
+            return response(json_encode($validate->errors()), 400)
+                            ->header('Content-Type', 'application/json');
+        }
+
         if( $investments->save() ){
-          return new InvestmentResource( $investments );
+          return new InvestmentResource($investments);
         }
     }
 

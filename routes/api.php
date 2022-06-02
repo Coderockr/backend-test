20c8efe7878ba90bc6b2c456155bda034d7144e7
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-02 18:42:29 -0300

    (feat): Creating the route to register a user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+
 Route::get('welcome', function (Request $request) {
-    return 'Hello World!';
+    return json_encode('Hello World!');
 });
 
 Route::get('welcome-auth', function (Request $request) {
-    return 'Hello World!';
+    return json_encode('Hello World!');
 })->middleware('auth:sanctum');

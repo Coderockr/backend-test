d58638c1326305f82d3edce7cf521931b3a91fa3
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-02 21:13:32 -0300

    (feat): Adding the Investment structure

---
+++ app/Models/Investment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Investment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'amount',
+        'created_at',        
+        'user_id'
+    ];
+}
---
+++ database/migrations/2022_06_02_221645_create_investments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 8, 2);
+            $table->date('created_at');
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+};

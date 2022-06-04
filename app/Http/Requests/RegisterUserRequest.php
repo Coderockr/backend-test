42b0ae1c3379f6e58af33b3da65644bf88360561
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-04 16:17:41 -0300

    (upgrade): Validation to user register

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed' 
+        ];
+    }
+}

db9f3cbb01f064adc569bf9443dad12f618152ac
Gabriel <gabriel.aconsurgens@gmail.com>
2022-06-03 21:08:53 -0300

    test: add get-withdrawal tests

--- src/test/integration-tests/withdrawal.test.ts
+++ src/test/integration-tests/withdrawal.test.ts
@@ -2,7 +2,7 @@ import { Application } from "express"
 import Server from '../../server'
 import supertest from 'supertest'
 import { disconnectMongoose } from '../helpers/database'
-import { makeInvestmentQuery, makeUserQuery, withdrawalInvestment } from "../helpers/query"
+import { getWithdrawalByInvestment, makeInvestmentQuery, makeUserQuery, withdrawalInvestment } from "../helpers/query"
 import { addMonths } from "date-fns"
 
 const appServer = new Server()
@@ -51,5 +51,43 @@ describe('Mutation - Withdrawal Investment', () => {
         })
 
         expect(result.body.errors).toBeTruthy()
+    })
+})
+
+describe('Query - Get Withdrawal by investment id', () => {
+    test('Should failed because investiment id is invalid', async () => {
+        const result = await agent.post('/graphql').send({
+            query: getWithdrawalByInvestment('invalid_id')
+        })
+    
+        expect(result.body.errors).toBeTruthy()
+    })
+
+    test('Should return a valid withdrawal with success', async () => {
+        const { body: bodyUser } = await agent.post('/graphql').send({ 
+            query: makeUserQuery() 
+          })
+          
+        const { body: bodyInvestment } = await agent.post('/graphql').send({ 
+            query: makeInvestmentQuery({
+                userId: bodyUser?.data?.createUser.id,  
+                initialValue: 200
+            })
+        })
+     
+        const investmentId = bodyInvestment?.data?.createInvestment?.id
+
+        await agent.post('/graphql').send({ 
+            query: withdrawalInvestment({
+                investmentId: investmentId
+            })
+        })
+     
+        const { body: bodyResult } = await agent.post('/graphql').send({
+            query: getWithdrawalByInvestment(investmentId)
+        })
+    
+        expect(bodyResult?.data?.getWithdrawalByInvestment).toBeInstanceOf(Object)
+
     })
 })

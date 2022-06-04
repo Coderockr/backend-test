e19678555ba02eaa9ab83b5dcc52a056eb965382
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-04 18:22:44 -0300

    (test): Generating new tests to api and service

---
+++ tests/Unit/InvestmentServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+use App\Services\InvestmentService;
+
+class InvestmentServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}

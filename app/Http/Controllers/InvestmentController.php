b66e07197d47cb5b58f65338c91c656ee11b5a1c
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-04 12:54:00 -0300

    (feat): Creating the withdrawal logic

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -16,8 +16,7 @@ class InvestmentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         $investments = auth()->user()->investments()->paginate(10);
 
         return response($investments, 200);
@@ -81,6 +80,52 @@ class InvestmentController extends Controller
         return response($response, 200);
     }
 
+    public function withdrawal($id) {
+        $investment = auth()->user()->investments()->find($id);
+        $investedAmount = $investment['amount'];
+
+        // Calculating how many times the gain fee was applied
+        $insertedAt = Carbon::createFromFormat('Y-m-d', $investment['inserted_at'])->hour(0)->minute(0)->second(0);
+        $currentDate = Carbon::now()->hour(0)->minute(0)->second(0);
+        $numberOfMonths = $insertedAt->diffInMonths($currentDate);
+
+        // Gain fees converted to decimal to use the compound gain formula
+        $gainFees = 0.0052;
+
+        // Casting to string to not use it in response as a float
+        $totalValue = $investedAmount * pow((1 + $gainFees), $numberOfMonths);
+
+        // Only the gain value
+        $gainValue =  $totalValue - $investedAmount;
+
+        $numberOfYears = $insertedAt->diffInYears($currentDate);
+
+        $taxPercent = match (true) {
+            $numberOfYears < 1 => 0.225,
+            $numberOfYears >= 1 && $numberOfYears < 2 => 0.185,
+            $numberOfYears >= 2 => 0.15,
+            default => 1
+        };
+
+        $moneyToReduce = $taxPercent * $gainValue;
+
+        $totalValue -= $moneyToReduce;
+
+        $response = [
+            'total' => $totalValue,
+            'taxPercent' => $taxPercent,
+            'taxValue' => $moneyToReduce,
+            'gain' => $gainValue,
+            'initial' => $investedAmount
+        ];
+        /*
+            (< 1 ano) =>
+            If it is less than one year old, the percentage will be 22.5% (tax = 45.00).
+            If it is between one and two years old, the percentage will be 18.5% (tax = 37.00).
+            If older than two years, the percentage will be 15% (tax = 30.00).
+        */
+        return response($response, 201);
+    }
     /**
      * Update the specified resource in storage.
      *

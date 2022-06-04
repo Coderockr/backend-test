06b298c65f6a8f6ff030cd7c8f294e2bebd7a01f
Gabriel <gabriel.aconsurgens@gmail.com>
2022-06-03 21:05:37 -0300

    fix: change date validation

--- src/modules/withdrawal/WithdrawalType.ts
+++ src/modules/withdrawal/WithdrawalType.ts
@@ -2,11 +2,12 @@ import { Field, InputType } from "type-graphql";
 import { Withdrawal } from "./WithdrawalModel";
 import { ObjectId } from "mongodb";
 import { MaxDate } from "class-validator";
+import { addHours } from "date-fns";
 
 @InputType()
 export class WithdrawalInput implements Partial<Withdrawal> {
   @Field()
-  @MaxDate(new Date())
+  @MaxDate(addHours(new Date(), 1))
   creationDate: Date;
 
   @Field(() => String)

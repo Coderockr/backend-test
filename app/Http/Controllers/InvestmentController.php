2b49daf6defa152b4ae4f881d33e9eb31faf1596
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-04 14:54:05 -0300

    (upgrade): Changing the location of the business logic

--- app/Http/Controllers/InvestmentController.php
+++ app/Http/Controllers/InvestmentController.php
@@ -50,7 +50,7 @@ class InvestmentController extends Controller
         $investment = auth()->user()->investments()->find($id);
 
         $investedService = new InvestmentService();
-        $expectedBalance = (string) $investedService->getCompoundGains($investment, Investment::GAIN_VALUE);
+        $expectedBalance = (string) $investedService->getExpectedBalance($investment);
 
         $response = [
             'initial_amount' => $investment->amount,
@@ -70,46 +70,9 @@ class InvestmentController extends Controller
         $investment = auth()->user()->investments()->find($id);
         $investedAmount = $investment->amount;
 
-        // Calculating how many times the gain fee was applied
-        $insertedAt = Carbon::createFromFormat('Y-m-d', $investment->inserted_at)->hour(0)->minute(0)->second(0);
-        $currentDate = Carbon::now()->hour(0)->minute(0)->second(0);
-        $numberOfMonths = $insertedAt->diffInMonths($currentDate);
-
-        // Gain fees converted to decimal to use the compound gain formula
-        $gainFees = 0.0052;
-
-        // Casting to string to not use it in response as a float
-        $totalValue = $investedAmount * pow((1 + $gainFees), $numberOfMonths);
-
-        // Only the gain value
-        $gainValue =  $totalValue - $investedAmount;
-
-        $numberOfYears = $insertedAt->diffInYears($currentDate);
-
-        $taxPercent = match (true) {
-            $numberOfYears < 1 => 0.225,
-            $numberOfYears >= 1 && $numberOfYears < 2 => 0.185,
-            $numberOfYears >= 2 => 0.15,
-            default => 1
-        };
-
-        $moneyToReduce = $taxPercent * $gainValue;
-
-        $totalValue -= $moneyToReduce;
-
-        $response = [
-            'total' => $totalValue,
-            'taxPercent' => $taxPercent,
-            'taxValue' => $moneyToReduce,
-            'gain' => $gainValue,
-            'initial' => $investedAmount
-        ];
-        /*
-            (< 1 ano) =>
-            If it is less than one year old, the percentage will be 22.5% (tax = 45.00).
-            If it is between one and two years old, the percentage will be 18.5% (tax = 37.00).
-            If older than two years, the percentage will be 15% (tax = 30.00).
-        */
-        return response($response, 201);
+        $investedService = new InvestmentService();
+        $expectedBalance = $investedService->getInvestmentReturn($investment);
+        
+        return response($expectedBalance, 201);
     }
 }

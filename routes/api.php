3546c7aef6d34e89ea3f41aee5f08bec8c879d3a
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-06-04 22:13:04 -0300

    (upgrade): Creating names to the routes

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->name('users.register');
+Route::post('/login', [AuthController::class, 'login'])->name('users.login');
 
 // Every route inside this block requires the sanctum authentication
 Route::group(['middleware' => ['auth:sanctum']], function() {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('users.logout');
     
     Route::apiResource('/investments', InvestmentController::class)->only(['index', 'show', 'store']);
     Route::post('/investments/{id}/withdrawal', [InvestmentController::class, 'withdrawal'])->name('investments.withdrawal');

60c32c89edf450524626d936e0de070c7690a9ae
Rodrigo Werlich <rodrigo.werlich@gmail.com>
2022-06-22 01:43:44 -0300

    sacar e exibir investimento

--- routes/investimento.php
+++ routes/investimento.php
@@ -5,4 +5,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->group('/investimento', function () use ($app) {
     $app->post('/', fn(Request $request, Response $response) => $this->ApiController->save($request, $response));
+    $app->get('/{owner}/{index}/', fn(Request $request, Response $response) => $this->ApiController->viewInvestiment($request, $response));
+    $app->get('/{id}/', fn(Request $request, Response $response) => $this->ApiController->viewInvestiment($request, $response));
+    $app->put('/{id}/', fn(Request $request, Response $response) => $this->ApiController->withdraw($request, $response));
 });

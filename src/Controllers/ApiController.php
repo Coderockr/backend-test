1e65fe40fe7c7f6779c6467abeee1ca8ac7175e0
Rodrigo Werlich <rodrigo.werlich@gmail.com>
2022-06-22 01:43:52 -0300

    sacar e exibir investimento

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -22,6 +22,28 @@ use App\Helpers\RequireValidator;
 class ApiController extends Controller
 {
 
+    private function calculateProfitValue(Investment $investment): float
+    {
+        $expectedValue = $investment->getInitialValue();
+        $end = $investment->getWithdrawalDate() ?? new \DateTime();
+        $dateInterval = $investment->getCreated()->diff($end);
+        $months = $dateInterval->m + ($dateInterval->y * 12);
+        for ($i = 0; $i < $months; $i++) {
+            $expectedValue += $expectedValue * 0.52 / 100;
+        }
+        return $expectedValue - $investment->getInitialValue();
+    }
+
+    private function calculateTaxValue(Investment $investment): float
+    {
+        $profit = $this->calculateProfitValue($investment);
+        $end = $investment->getWithdrawalDate();
+        $dateInterval = $investment->getCreated()->diff($end);
+        if ($dateInterval->y < 1) return $profit * 22.5 / 100;
+        elseif ($dateInterval->y < 2) return $profit * 18.5 / 100;
+        else return $profit * 15 / 100;
+    }
+
     public function save(Request $request, Response $response)
     {
         try {
@@ -47,6 +69,65 @@ class ApiController extends Controller
                 'status' => 'ok',
                 'id' => $investment->getId(),
             ], 201)
+                ->withHeader('Content-type', 'application/json');
+        } catch (Exception $e) {
+            $this->em->rollback();
+            return $response->withJson([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ])->withStatus(500);
+        }
+    }
+
+    public function viewInvestiment(Request $request, Response $response)
+    {
+        try {
+            $id = $request->getAttribute('route')->getArgument('id');
+            $investment = $this->em->getRepository(Investment::class)->find($id);
+            return $response->withJson([
+                'status' => 'ok',
+                'initialValue' => Utils::formatMoney($investment->getInitialValue()),
+                'profitValue' => Utils::formatMoney($this->calculateProfitValue($investment)),
+                'expectedValue' => Utils::formatMoney($investment->getInitialValue() + $this->calculateProfitValue($investment)),
+            ], 200)
+                ->withHeader('Content-type', 'application/json');
+        } catch (Exception $e) {
+            $this->em->rollback();
+            return $response->withJson([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ])->withStatus(500);
+        }
+    }
+
+    public function withdraw(Request $request, Response $response)
+    {
+        try {
+            $this->em->beginTransaction();
+            $data = (array)$request->getParsedBody();
+            $fields = [
+                'withdrawDate' => 'Data de saque',
+            ];
+            Validator::requireValidator($fields, $data);
+            Validator::validDate($data['withdrawDate']);
+            $withdrawDate = \DateTime::createFromFormat('d/m/Y', $data['withdrawDate']);
+            $id = $request->getAttribute('route')->getArgument('id');
+            $investment = $this->em->getRepository(Investment::class)->find($id);
+            if ($investment->getWithdrawalDate() != null) throw new \Exception('Esse investimento já foi sacado');
+            if ($withdrawDate->format('Y-m-d') > date('Y-m-d')) throw new \Exception('A data não pode ser maior que o dia atual');
+            if ($withdrawDate->format('Y-m-d') < $investment->getCreated()->format('Y-m-d')) throw new \Exception("Data minima {$investment->getCreated()->format('d/m/Y')}");
+            $investment->setWithdrawalDate($withdrawDate)
+                ->setTax($this->calculateTaxValue($investment))
+                ->setProfit($this->calculateProfitValue($investment));
+            $this->em->getRepository(Investment::class)->save($investment);
+            $this->em->commit();
+            return $response->withJson([
+                'status' => 'ok',
+                'initialValue' => Utils::formatMoney($investment->getInitialValue()),
+                'profitValue' => Utils::formatMoney($investment->getProfit()),
+                'taxValue' => Utils::formatMoney($investment->getTax()),
+                'withdrawValue' => Utils::formatMoney($investment->getWithdrawValue()),
+            ], 200)
                 ->withHeader('Content-type', 'application/json');
         } catch (Exception $e) {
             $this->em->rollback();

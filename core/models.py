af9f609d122bbe934665ba17d34e103fb1dfad57
João Prado <joaopedro0128@hotmail.com>
2022-08-15 16:54:14 -0300

    feat: creates investment model

--- core/models.py
+++ core/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
   ...
+
+
+class Investment(models.Model):
+  owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  amount = models.FloatField()
+  active = models.BooleanField(default=True, null=False)
+  created_at = models.DateTimeField()

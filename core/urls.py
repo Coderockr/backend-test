882b920f39cb4908521ddab12a520173870593e2
João Prado <joaopedro0128@hotmail.com>
2022-08-16 00:56:03 -0300

    feat: improves user viewset

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import UserViewSet
 
 
-router = DefaultRouter()
-router.register('user', UserViewSet)
+router = SimpleRouter()
+router.register('users', UserViewSet)
 
 urlpatterns = [
+  path(r'login/', obtain_auth_token),
   path('', include(router.urls))
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,30 @@
-from http import HTTPStatus
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (CreateModelMixin, UpdateModelMixin,
+                                   DestroyModelMixin, RetrieveModelMixin)
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .permissions import IsOwnProfile
 from .serializers import UserSerializer
 from .models import User
 
 # Create your views here.
-class UserViewSet(ModelViewSet):
+class UserViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin,
+                  GenericViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
   permission_classes = (IsOwnProfile,)
+
+  def retrieve(self, request, *args, **kwargs):
+    if not request.user.id:
+      return Response()
+    serializer = self.get_serializer(request.user)
+    return Response(serializer.data)
+
+  @action(methods=['get'], detail=False)
+  def whoami(self, request):
+    if not request.user.id:
+      return Response()
+    serializer = self.get_serializer(data=request.user)
+    serializer.is_valid(raise_exception=True)
+    return Response(serializer.data)

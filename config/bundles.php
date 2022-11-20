b8ee7c315134c741faeb3af6709bd81522e4e4be
luan441 <luanhenrique441@gmail.com>
2022-11-20 20:33:06 -0300

    :heavy_plus_sign: build: dependencia de seguranca do symfony

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];

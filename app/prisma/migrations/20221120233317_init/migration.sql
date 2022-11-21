4817919581f769331dd8899b0f7c760c6b0c471d
junior barros <juniorbaarros@gmail.com>
2022-11-20 22:52:44 -0300

    feat: updated prisma schema and generated migration file

---
+++ app/prisma/migrations/20221120233317_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "owners" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(60) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "owners_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "investiments" (
+    "id" SERIAL NOT NULL,
+    "owner_id" INTEGER NOT NULL,
+    "creation_date" TIMESTAMPTZ NOT NULL,
+    "initial_amount" MONEY NOT NULL,
+    "amount" MONEY NOT NULL,
+
+    CONSTRAINT "investiments_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "investiments" ADD CONSTRAINT "investiments_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "owners"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

9dd86098b52ea4721fa4b371f1da0ead3f35666e
luan441 <luanhenrique441@gmail.com>
2022-11-21 18:05:58 -0300

    :heavy_plus_sign: build: instalacao bundle de JWT do symfony

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];

2233c0ce4b5d4c81575d318df7660364b7617b66
thalesmengue <thalesmmachado@gmail.com>
2022-11-21 12:47:58 -0300

    feat: command to create a random investment

---
+++ app/Console/Commands/CreateRandomInvestment.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Investment;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateRandomInvestment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:investment {amount} {user_id} {date}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a random investment';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $amount = $this->argument('amount');
+        $user_id = $this->argument('user_id');
+        $date = Carbon::createFromFormat('Y-m-d', $this->argument('date'));
+
+        DB::table('investments')->insert([
+            'amount' => $amount,
+            'user_id' => $user_id,
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+
+    }
+}

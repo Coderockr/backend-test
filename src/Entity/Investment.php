d8da0e21f38de37b3ddd0cba813cf053e3333620
luan441 <luanhenrique441@gmail.com>
2022-11-21 19:41:43 -0300

    :sparkles: feat: entidade de investimento

---
+++ src/Entity/Investment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvestmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvestmentRepository::class)]
+class Investment
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+	#[ORM\GeneratedValue(strategy: 'CUSTOM')]
+	#[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    #[ORM\Column]
+    private float $value;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
+    private ?\DateTimeImmutable $dateOfWithdrawl = null;
+
+	public function __construct(
+		User $user,
+		float $value,
+		\DateTimeImmutable $createdAt
+	)
+	{
+		$this->user = $user;
+		$this->value = $value;
+		$this->createdAt = $createdAt;
+	}
+}

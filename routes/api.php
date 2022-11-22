097ee5ea4c6743ce828795d2c18789ddd896e25c
Daniel Carlos Soares <danielcarlossoares@gmail.com>
2022-11-22 17:22:16 -0300

    Person's investments endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\PersonController;
 use App\Http\Controllers\InvestmentController;
 use App\Http\Controllers\InvestmentMovementController;
 use App\Http\Controllers\InvestmentWithdrawnController;
+use App\Http\Controllers\PersonInvestmentsController;
 
 Route::prefix('v1')->group(function() {
     Route::resource('persons', PersonController::class, ['except' => ['create', 'edit']]);
+    Route::get('persons/{id}/investments', [PersonInvestmentsController::class, 'index']);
+
     Route::resource('investments', InvestmentController::class, ['except' => ['create', 'edit']]);
-    Route::get('investments/{id}/movements', [InvestmentMovementController::class, 'index']);
+    Route::get('investments/{id}/movements', [InvestmentMovementController::class, 'index'])
+        ->name('investments.movements.index');
+
     Route::patch('investments/{id}/withdrawn', [InvestmentWithdrawnController::class, 'withdrawn']);
 });

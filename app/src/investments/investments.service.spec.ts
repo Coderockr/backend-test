58fe0b87cf5c2d111d0cc820a148d1efe82a928f
Junior Barros <juniorbaarros@gmail.com>
2022-11-22 19:14:49 -0300

    chore: packages for mock test and prismaORM

--- app/src/investments/investments.service.spec.ts
+++ app/src/investments/investments.service.spec.ts
@@ -1,18 +1,22 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvestmentsService } from './investments.service';
+import { OwnersModule } from '../owners/owners.module';
+import { MailModule } from '../mail/mail.module';
 
 describe('InvestmentsService', () => {
-  let service: InvestmentsService;
+  let investmentService: InvestmentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [OwnersModule, MailModule],
       providers: [InvestmentsService],
     }).compile();
 
-    service = module.get<InvestmentsService>(InvestmentsService);
+    investmentService = module.get<InvestmentsService>(InvestmentsService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(investmentService).toBeDefined();
   });
 });

fa52e07f9126073753a01fb9d81ce1eed5b8743f
Felipe Ramon <rammonfelip@gmail.com>
2022-11-23 03:02:08 -0300

    Add a simple bootstrap layout.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+
+@section('h1', 'Dashboard')
+
+@section('content')
+    <div class="row">
+        <h1 class="mt-5">Welcome to the investment application!</h1>
+        <p class="lead">To see the documentation, please visit <small><a href="javascript:;">Documentation Page</a></small></p>
+        <p>Enjoy.</p>
+    </div>
+@endsection

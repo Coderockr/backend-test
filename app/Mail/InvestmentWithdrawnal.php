8d25ba2e189830fd17a9e573bd49a11d534fa413
Daniel Carlos Soares <51865645+DanielCarlos-hub@users.noreply.github.com>
2022-11-22 23:00:12 -0300

    fix view file path

--- app/Mail/InvestmentWithdrawnal.php
+++ app/Mail/InvestmentWithdrawnal.php
@@ -46,7 +46,7 @@ class InvestmentWithdrawnal extends Mailable
     public function content()
     {
         return new Content(
-            view: 'view.mail.investment',
+            view: 'mail.investment',
         );
     }
 

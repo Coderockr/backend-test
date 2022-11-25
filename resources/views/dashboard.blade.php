7c799a44699890e04151df781222b2af2da3a941
Felipe Ramon <rammonfelip@gmail.com>
2022-11-25 00:38:12 -0300

    Updating readme, add instructions.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="row">
         <h1 class="mt-5">Welcome to the investment application!</h1>
-        <p class="lead">To see the documentation, please visit <small><a href="javascript:;">Documentation Page</a></small></p>
+        <p class="lead">To see the documentation, please visit <small><a target="_blank" href="https://documenter.getpostman.com/view/24612120/2s8YsqVaS6">Documentation Page</a></small></p>
         <p>Enjoy.</p>
     </div>
 @endsection

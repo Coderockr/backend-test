f548c5fd010a3a661619f40232dc814ee93b3f90
Cayo Nakasato <cayonakasato112@gmail.com>
2022-12-07 11:06:57 -0400

    feat: can list one investment, paginated

--- investments/urls.py
+++ investments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from investments.views import CreateInvestmentView, ListInvestmentView, RetrieveUpdateDestroyInvestmentDetailView, WithdrawnInvestmentView
+from investments.views import CreateInvestmentView, ListInvestmentView, ListOneUserInvestmentsView, RetrieveUpdateDestroyInvestmentDetailView, WithdrawnInvestmentView
 
 
 urlpatterns=[
     path('investments/', ListInvestmentView.as_view()),
     path('investments/<str:owner_id>/', CreateInvestmentView.as_view()),
+    path('investments/<str:owner_id>/management/', ListOneUserInvestmentsView.as_view()),
     path('investment/<str:investment_id>/', RetrieveUpdateDestroyInvestmentDetailView.as_view()),
     path('investment/<str:investment_id>/withdrawn/', WithdrawnInvestmentView.as_view()),
 

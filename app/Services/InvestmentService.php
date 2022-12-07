b63aab95b5875e2d308fe00325b0703e8c4ad646
Bruno Holanda <br.holanda15@gmail.com>
2022-12-07 19:54:27 -0300

    Returned id when creating investment

--- app/Services/InvestmentService.php
+++ app/Services/InvestmentService.php
@@ -33,13 +33,13 @@ class InvestmentService
 
     public function create(array $data)
     {
-        Investment::create([
+        $investment = Investment::create([
             'initial_amount' => $data['amount'],
             'creation_date' => $data['creation_date'],
             'owner_id' => $data['owner_id']
         ]);
 
-        return response()->json('Created Investment!', 201);
+        return response()->json("Created Investment - id: {$investment->id}", 201);
     }
 
     public function withdrawal(array $data)

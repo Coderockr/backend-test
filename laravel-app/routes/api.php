c45eb2af50f9b9f4372711a020a64bce16aac0bd
Caio <bladellano@gmail.com>
2023-03-25 09:46:17 -0300

    [CODE-001] Criando controllers Owner; Investment e adicionando rotas

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\InvestmentController;
+use App\Http\Controllers\OwnerController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('owners',OwnerController::class);
+
+Route::resource('investments',InvestmentController::class);

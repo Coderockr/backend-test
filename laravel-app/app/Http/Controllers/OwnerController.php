cea0f2cc0ac5c290d13145111357ac8a83e41824
Caio <bladellano@gmail.com>
2023-03-25 10:41:25 -0300

    [CODE-004] Implementando métodos em OwnerController e add rota p/ listar somente investimentos de um proprietário c/ paginação

--- laravel-app/app/Http/Controllers/OwnerController.php
+++ laravel-app/app/Http/Controllers/OwnerController.php
@@ -2,50 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Owner;
+use App\Models\Investment;
 use Illuminate\Http\Request;
 
 class OwnerController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return 'Hello!';
+        $search = $request->input('search');
+
+        $owners = Owner::with('investments')
+        ->where('id',$search)
+        ->orWhere('email', 'like', '%'.$search.'%')
+        ->paginate(10);
+
+        return response()->json($owners);
+    }
+
+    /**
+     * Display the specified resource.
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Owner $owner)
+    {
+      $owner->investments;
+      return response()->json($owner);
+    }
+
+    /**
+     * Returns only owner investments
+     * @param Owner $owner
+     * @return \Illuminate\Http\Response
+     */
+    public function onlyInvestments(Owner $owner)
+    {
+      $investments = Investment::where('owner_id', $owner->id)->paginate(10);
+      return response()->json($investments);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Owner $owner)
+    {
+        $owner->delete();
+        return response()->json(['message'=> 'Owner and investments successfully removed!']);
     }
 
     /**
      * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
     /**
@@ -56,12 +82,10 @@ class OwnerController extends Controller
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -71,14 +95,5 @@ class OwnerController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+
 }
--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -10,6 +10,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Owners
 Route::resource('owners',OwnerController::class);
+Route::get('owners/only-investments/{owner}',[OwnerController::class,'onlyInvestments']);
 
+//Investments
 Route::resource('investments',InvestmentController::class);
